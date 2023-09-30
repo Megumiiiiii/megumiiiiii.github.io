c414e13336a68c4197d9d85d0ada128e4c4e8b23
Megumiiiiii <megumii6976@gmail.com>
2023-09-30 17:49:24 +0700

    A

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Megumiiiiii', // Usually your GitHub org/user name.
   projectName: 'megumiiiiii.github.io', // Usually your repo name.
-
+  deploymentBranch: 'main',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
